1fe3c4cc25e046d9e2576dcde99843e68ca0fd88
hiteshahire98 <hituahire1998@gmail.com>
2022-10-20 13:02:34 +0530

    c

---
+++ Collections/ArrayListDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Collections
+//{
+//    internal class ArrayListDemo
+//    {
+//        static void Main(string[] args)
+//        {
+//            ArrayList list = new ArrayList();
+//            list.Add(10);  // boxing
+//            list.Add(22.22);
+//            list.Add("hello");
+
+//            double d = Convert.ToDouble(list[1]);
+//            int a = Convert.ToInt32(list[0]);  // unboxing
+
+//            foreach (var item in list)
+//            {
+//                Console.WriteLine(item);
+//            }
+
+//        }
+//    }
+//}
