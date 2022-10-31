ef88a01feca68e69b8642da8166404a8876656f4
hiteshahire98 <hituahire1998@gmail.com>
2022-10-31 11:26:15 +0530

    c

---
+++ Collections/GenericClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Collections
+//{
+//    internal class GenericClass
+//    {
+//        public class MyGeneric<T>  // T - placeholder for the type
+//        {
+//            private T data;        
+
+//            public void Accept(T data)
+//            {
+//                this.data = data;
+//            }
+
+//            public T GetData()
+//            {
+//                return data;
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            MyGeneric<int> myGeneric1 = new MyGeneric<int>();
+//            myGeneric1.Accept(10);
+
+//          //  MyGeneric<Employee> myGeneric2 = new MyGeneric<Employee>();
+//          //  myGeneric2.Accept(new Employee("test", 20000));
+
+
+//            MyGeneric<string> s = new MyGeneric<string>();
+//            s.Accept("Hello");
+//        }
+//    }
+//}
